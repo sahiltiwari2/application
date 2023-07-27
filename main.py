329da3629a54630cb77d1715f72ce49227fe665a
SK Gokulbarath <64578167+gokul6350@users.noreply.github.com>
2023-07-27 21:13:13 +0530

    added class funtion

--- main.py
+++ main.py
@@ -6,89 +6,64 @@ import math
 customtkinter.set_appearance_mode('light')
 customtkinter.set_default_color_theme("dark-blue")
 
-root = customtkinter.CTk()
-root.title("Focus application")
-root.geometry("700x500")
-
-global username_input
-global password_input
 
 
 
+class Appliaction(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-
-
-def sideframe2_clock():
-    side_frame2 = customtkinter.CTkFrame(master = frame, fg_color = "#E3E3E3") 
-    side_frame2.pack(padx = 10, pady = 10, fill ="both", expand = True)
-
-    for widgets in side_frame2.winfo_children():# to clear the frame
-        widgets.destroy()
-
-
-    analog_canvas = tk.Canvas(side_frame2,width = 400, height = 400, bg = 'grey') # '#E3E3E3' color
-    analog_canvas.pack()
-
-    digital_canvas = tk.Canvas(side_frame2, width = 400, height =100, bg = "#E3E3E3")
-    digital_canvas.pack()
-
-def sideframe2_timer():
-    side_frame2 = customtkinter.CTkFrame(master = frame, fg_color = "#E3E3E3") 
-    side_frame2.pack(padx = 10, pady = 10, fill ="both", expand = True)
-
-
-    analog_canvas = tk.Canvas(side_frame2,width = 400, height = 400, bg = 'red') # '#E3E3E3' color
-    analog_canvas.pack()
-
-    digital_canvas = tk.Canvas(side_frame2, width = 400, height =100, bg = "red")
-    digital_canvas.pack()
+        self.title("Focus application")
+        self.geometry("1080x900")
+        frame = customtkinter.CTkFrame(master = self)
+        frame.pack(padx = 20, pady = 20, fill = "both", expand = True)
+        self.main_app(frame)
 
 
 
 
 
-def main_app():
+
+    def main_app(self, frame):
 
 
-    side_frame = customtkinter.CTkFrame(master = frame, fg_color= "#D0F4FF") # this is a bad way to crate a side frame things will go wrong when we resize the window
-    side_frame.pack(side = customtkinter.LEFT)
-    side_frame.pack_propagate(False)
-    side_frame.configure(width = 250, height = 1000)
+        side_frame = customtkinter.CTkFrame(master = frame, fg_color= "#D0F4FF") # this is a bad way to crate a side frame things will go wrong when we resize the window
+        side_frame.pack(side = customtkinter.LEFT)
+        side_frame.pack_propagate(False)
+        side_frame.configure(width = 250, height = 1000)
 
-    label = customtkinter.CTkLabel(master= side_frame, text = "" )
-    label.pack(pady = 20)
+        label = customtkinter.CTkLabel(master= side_frame, text = "" )
+        label.pack(pady = 20)
 
-    focus_button = customtkinter.CTkButton(master = side_frame, 
-                                   text = "Focus session", 
-                                   font = ("Arial", 20),
-                                   fg_color = '#D0F4FF', 
-                                   text_color= 'black', 
-                                   width = 250,
-                                   height = 60,
-                                   hover_color = "#9BB5BD",
-                                   #command= sideframe2_timer
-                                   )
-    focus_button.pack()
+        focus_button = customtkinter.CTkButton(master = side_frame, 
+                                       text = "Focus session", 
+                                       font = ("Arial", 20),
+                                       fg_color = '#D0F4FF', 
+                                       text_color= 'black', 
+                                       width = 250,
+                                       height = 60,
+                                       hover_color = "#9BB5BD",
+                                    #command= sideframe2_timer
+                                        )
+        focus_button.pack()
                                        
         
-    clock_button = customtkinter.CTkButton(master= side_frame, 
-                                   text = "Clock", 
-                                   font = ("Arial", 20),
-                                   fg_color = '#D0F4FF', 
-                                   text_color= 'black', 
-                                   width = 250,
-                                   height = 60,
-                                   hover_color = "#9BB5BD",
-                                   #command= sideframe2_clock
-                                   )
-    clock_button.pack()
+        clock_button = customtkinter.CTkButton(master= side_frame, 
+                                       text = "Clock", 
+                                       font = ("Arial", 20),
+                                       fg_color = '#D0F4FF', 
+                                       text_color= 'black', 
+                                       width = 250,
+                                       height = 60,
+                                       hover_color = "#9BB5BD",
+                                       #command= sideframe2_clock
+                                       )
+        clock_button.pack()
 
 
 
 
-frame = customtkinter.CTkFrame(master = root)
-frame.pack(padx = 20, pady = 20, fill = "both", expand = True)
+if __name__=="__main__":
 
-
-main_app()
-root.mainloop()
+    app = Appliaction()
+    app.mainloop()
